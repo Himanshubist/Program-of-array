03801097a76b5e95b6860a4bbbaff66fcb4cd610
himanshubist <himanshubisht111@gmail.com>
2022-05-20 00:56:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sumOfnumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            int[] numbers = new int[10];
+
+            for(n=0;n<10;n++)
+            {
+                Console.WriteLine($"Enter {n+1} number: ");
+                numbers[n] = int.Parse(Console.ReadLine());
+                
+                sum += numbers[n];
+            }
+            Console.WriteLine($"Sum of all {n} numbers stored in array is : {sum}");
+            Console.ReadLine();
+        }
+        
+
+    }
+
+
+
+}
